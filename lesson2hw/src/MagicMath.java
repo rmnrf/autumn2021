1e92de67ff8d5330bd8341182fcbd62cd156b9b0
Roman <getmepower@protonmail.com>
2021-10-25 16:19:50 +0400

    lesson2 hw

---
+++ lesson2hw/src/MagicMath.java
@@ -0,0 +1,21 @@
+public class MagicMath {
+    public static int fact(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static int abs(int n) {
+        return (n < 0) ? -n : n;
+    }
+
+    public static int pow(int n, int e) {
+        int result = 1;
+        for (int i = 1; i <= e; i++) {
+            result *= n;
+        }
+        return result;
+    }
+}
